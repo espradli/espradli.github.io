0d1778dd172eac222a95355c45b31447ec45bd17
Emerson Spradling <34756081+espradli@users.noreply.github.com>
2026-03-29 23:01:17 -0800

    Moving disable to JS.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import React from 'react';
-import './App.sass';
-//Component Imports
-import Title from './components/title'
-import IconTray from './components/iconTray'
-import AboutMe from './components/aboutMe';
-import Projects from './components/projects'
-import Resume from './components/resume'
-// import Navigation from './components/navigation'
-
-
 function App() {
 
   return (    
     <div className='App'>
-      <div className="Background">
-          {/* <Navigation /> */}
-          <Title />
-          <IconTray />
-        </div>
-        <div className="Background">
-          <AboutMe />
-          <div style={{height: '10vh'}} />
-          <Projects />
-          <div style={{height: '10vh'}} />
-          <Resume />
-        </div>
+      WORK IN PROGRESS - 2026
     </div>
   );
 }
